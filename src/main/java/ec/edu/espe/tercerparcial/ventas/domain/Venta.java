e38513b7fa3a4f0dda9aa0258b826eeb0f11da54
RUBEN ALFREDO TOMALA VERA <ruben.al97@hotmail.com>
2024-03-11 20:50:23 -0500

    Commit 20:50

--- src/main/java/ec/edu/espe/tercerparcial/ventas/domain/Venta.java
+++ src/main/java/ec/edu/espe/tercerparcial/ventas/domain/Venta.java
@@ -1,2 +1,33 @@
-package ec.edu.espe.tercerparcial.ventas.domain;public class Venta {
+package ec.edu.espe.tercerparcial.ventas.domain;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Document(collection = "ventas")
+public class Venta {
+
+    @Field("id_venta")
+    private String idProducto;
+
+    @Field("fecha_venta")
+    private Date fechaVenta;
+
+    @Field("codigo_unico")
+    private String codigoUnico;
+
+    @Field("nombre_producto")
+    private String nombreProducto;
+
+    @Field("precio_unitario")
+    private String precioUnitario;
 }
